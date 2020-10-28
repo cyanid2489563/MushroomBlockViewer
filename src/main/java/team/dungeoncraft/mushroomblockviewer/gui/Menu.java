effe7468f24e85d7d81bdb55337335939bcf258c
Cyanide <slm789456123@gmail.com>
2020-10-28 19:59:22 +0800

    refactor: 移除不必要的程式碼

--- src/main/java/team/dungeoncraft/mushroomblockviewer/gui/Menu.java
+++ src/main/java/team/dungeoncraft/mushroomblockviewer/gui/Menu.java
@@ -7,10 +7,7 @@ import com.github.stefvanschie.inventoryframework.pane.Pane;
 import com.github.stefvanschie.inventoryframework.pane.StaticPane;
 import org.bukkit.Material;
 import org.bukkit.entity.Player;
-import org.bukkit.inventory.ItemFlag;
 import org.bukkit.inventory.ItemStack;
-import org.bukkit.inventory.meta.Damageable;
-import org.bukkit.inventory.meta.ItemMeta;
 import team.dungeoncraft.mushroomblockviewer.customblock.CustomBlockManager;
 import team.dungeoncraft.mushroomblockviewer.MushroomBlockViewer;
 import team.dungeoncraft.mushroomblockviewer.utility.CustomBlockConverter;
@@ -80,16 +77,5 @@ public final class Menu {
                     gui.update();
                 }), 0, 5);
         return staticPane;
-    }
-
-    public static ItemStack getCustomBlockItem(int counter) {
-        ItemStack itemStack = new ItemStack(Material.TURTLE_HELMET);    // 自訂義方塊的 Item 是用海龜頭盔做的
-
-        ItemMeta itemMeta = itemStack.getItemMeta();
-        itemMeta.setUnbreakable(true);
-        ((Damageable) itemMeta).setDamage(counter);
-        itemMeta.addItemFlags(ItemFlag.HIDE_ATTRIBUTES, ItemFlag.HIDE_UNBREAKABLE, ItemFlag.HIDE_DESTROYS);
-        itemStack.setItemMeta(itemMeta);
-        return itemStack;
     }
 }
